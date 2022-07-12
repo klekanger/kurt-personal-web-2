ec571bd2c1c65cc08140eb489a51436af4381acc
Kurt Lekanger <kurt@lekanger.no>
2022-07-12 15:11:04 +0200

    style: improve on small screens

--- apps/web/components/UI/featured-blog-posts.tsx
+++ apps/web/components/UI/featured-blog-posts.tsx
@@ -1,48 +1,55 @@
-import Container from './container';
-import Link from 'next/link';
-import { Post } from '../../types/interfaces';
 import Image from 'next/image';
+import Link from 'next/link';
 import { imageBuilder } from '../../lib/sanity';
+import { Post } from '../../types/interfaces';
 import HeadingWithMarks from '../content-renderers/heading-with-marks';
-import { BLURDATA } from '../../lib/blurdata';
+import Container from './container';
 
 export default function FeaturedBlogPosts({ content }: { content: Post[] }) {
   return (
     <Container>
-      <div className='default-spacing relative'>
+      <div className='default-spacing relative pb-0'>
         <div className='pb-4 md:pb-8'>
           <HeadingWithMarks heading='Siste <mark>blogginnlegg</mark>' />
         </div>
-        <div className='grid-row grid gap-16 lg:grid-cols-2 4xl:grid-cols-4'>
+        <div className='grid grid-rows-2 md:grid-cols-2 md:gap-16 4xl:grid-cols-4'>
           {content.map((post) => (
-            <div
-              key={post._id}
-              className='relative aspect-[4/3] w-full overflow-hidden rounded-lg shadow-lg shadow-brand-secondary1 dark:shadow-none'
-            >
-              <Image
-                className='rounded-lg'
-                layout='fill'
-                objectFit='cover'
-                src={
-                  imageBuilder(post?.mainImage).width(800).height(450).url() ||
-                  '#'
-                }
-                alt={post?.mainImage?.alt}
-                priority={true}
-                placeholder='blur'
-                blurDataURL={BLURDATA}
-              />
+            <div className='relative' key={post._id}>
+              <div className='relative aspect-[4/3] w-full rounded-lg shadow-lg shadow-brand-secondary1 dark:shadow-none'>
+                <Image
+                  className='rounded-lg'
+                  layout='fill'
+                  objectFit='cover'
+                  src={
+                    imageBuilder(post?.mainImage)
+                      .width(800)
+                      .height(450)
+                      .url() || '#'
+                  }
+                  alt={post?.mainImage?.alt}
+                />
+              </div>
 
-              <div className='absolute left-0 bottom-0 m-auto w-full transform rounded-b-lg bg-brand-black/50 bg-gradient-to-t py-6 text-brand-white backdrop-blur-lg duration-500 hover:text-brand-secondary1 dark:bg-brand-dark-main2/60 md:px-6 md:shadow-lg'>
-                <Link href={`/blog/${post.slug.current}`}>
-                  <a>
-                    <h2 className='mt-0 px-4 md:px-0 md:text-left lg:text-2xl'>
-                      {post.title.length > 90
-                        ? post.title.slice(0, 90).concat('[...]')
-                        : post.title}
-                    </h2>
-                  </a>
-                </Link>
+              <div className='left-0 m-auto w-full px-0'>
+                <div className='transform rounded-b-lg py-6 duration-500 hover:text-brand-secondary1 dark:bg-brand-dark-main2/60 md:absolute md:bottom-0 md:bg-brand-black/50 md:bg-gradient-to-t md:px-6 md:text-brand-white md:shadow-lg md:backdrop-blur-lg'>
+                  <Link href={`/blog/${post.slug.current}`}>
+                    <a>
+                      <h2 className='mt-0  md:text-left lg:text-2xl'>
+                        {post.title.length > 90
+                          ? post.title.slice(0, 90).concat('[...]')
+                          : post.title}
+                      </h2>
+                    </a>
+                  </Link>
+                  <p className='mt-2 md:hidden md:text-brand-dark-white'>
+                    {post.excerpt &&
+                      `${post.excerpt[0]?.children[0]?.text.slice(0, 500)}${
+                        post.excerpt[0]?.children[0]?.text.length > 500
+                          ? '...'
+                          : ''
+                      }`}
+                  </p>
+                </div>
               </div>
             </div>
           ))}
