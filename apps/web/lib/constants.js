89880d61601c07ff8e95a8cea326ebb988c0578d
Kurt Lekanger <kurt@lekanger.no>
2022-07-14 10:53:14 +0200

    SEO: use jpg instead of webp for og:image, as FB debugger complains

--- apps/web/lib/constants.js
+++ apps/web/lib/constants.js
@@ -1,3 +1,3 @@
-export const HOME_OG_IMAGE_URL = '/kurt-lekanger-tekst-og-kode-og-image.webp';
+export const HOME_OG_IMAGE_URL = '/kurt-lekanger-tekst-og-kode-og-image.jpg';
 
 export const CMS_URL = 'https://www.lekanger.no';
