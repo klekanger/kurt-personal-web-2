95255246d8afe5f1e2bfbbc8c0fa693584c7a83c
Kurt Lekanger <kurt@lekanger.no>
2022-07-18 14:52:02 +0200

    chore: custom 404

---
+++ apps/web/pages/404.js
@@ -0,0 +1,16 @@
+import Container from '../components/UI/container';
+import Layout from '../components/UI/layout';
+
+export default function Custom404() {
+  return (
+    <Layout>
+      <Container>
+        <div className='text-center'>
+          <div className='pb-4 text-9xl'>🤯</div>
+          <h1>Siden ble ikke funnet (404)</h1>
+          <p className='pt-4'>Nå har du rotet det til for deg selv...</p>
+        </div>
+      </Container>
+    </Layout>
+  );
+}
