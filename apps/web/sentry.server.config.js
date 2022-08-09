64305d1eaa67ff99e4bac2da26d607025c90dd03
Kurt Lekanger <kurt@lekanger.no>
2022-08-09 13:49:54 +0200

    chore: set Sentry DSN to null when in development environment

--- apps/web/sentry.server.config.js
+++ apps/web/sentry.server.config.js
@@ -4,10 +4,13 @@
 
 import * as Sentry from '@sentry/nextjs';
 
-const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+const SENTRY_DSN =
+  process.env.NODE_ENV === 'development'
+    ? null
+    : process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://726c63624cc14675ada692eade7cb188@o849093.ingest.sentry.io/5816155',
+  dsn: SENTRY_DSN,
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
